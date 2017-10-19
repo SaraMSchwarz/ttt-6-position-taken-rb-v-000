83bdb73d2500c8abc36d544610171d2cd656a59d
Sara Schwarz <saramschwarz@gmail.com>
2017-10-19 02:19:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
   taken = nil
 end
 if (board[index] ==  "  " || board[index] == " " || board[index] == nil)
-  then false
-else board(index) == " X " || " O "
-  then true
+  taken = false
+else
+  taken = true
 end
